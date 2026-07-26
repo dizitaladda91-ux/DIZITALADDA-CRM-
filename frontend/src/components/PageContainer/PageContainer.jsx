1f8077349d7b5a457726efbb695c8941baf065e0
satyam07july-png <satyam07july@gmail.com>
2026-07-26 13:57:27 +0530

    change env

--- frontend/src/components/PageContainer/PageContainer.jsx
+++ frontend/src/components/PageContainer/PageContainer.jsx
@@ -4,7 +4,7 @@ const PageContainer = ({ children }) => {
       <div className="mx-auto w-full px-4 py-4 sm:px-6 sm:py-6 lg:px-8 lg:py-8 xl:px-10 xl:py-8">
         {children}
       </div>
-    </main>
+    </div>
   );
 };
 
