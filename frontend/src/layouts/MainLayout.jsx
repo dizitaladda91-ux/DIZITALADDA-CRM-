c7cf0bed18b0a9180fd607b4f67f19eca8c56541
satyam07july-png <satyam07july@gmail.com>
2026-07-26 14:04:37 +0530

    change env

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -5,24 +5,38 @@ import Sidebar from "../components/layout/sidebar/Sidebar";
 import Topbar from "../components/layout/Topbar/Topbar";
 import PageContainer from "../components/PageContainer/PageContainer";
 
+const SIDEBAR_WIDTH = 280;
+
 const MainLayout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="flex min-h-screen bg-slate-100">
+    <div className="min-h-screen bg-slate-100">
+      {/* Sidebar */}
       <Sidebar
         isOpen={sidebarOpen}
         onToggle={() => setSidebarOpen(false)}
       />
 
-      <div className="flex flex-1 flex-col md:ml-[280px]">
+      {/* Main Content */}
+      <div
+        className="flex min-h-screen flex-col transition-all duration-300"
+        style={{
+          marginLeft:
+            typeof window !== "undefined" && window.innerWidth >= 768
+              ? `${SIDEBAR_WIDTH}px`
+              : "0px",
+        }}
+      >
+        {/* Topbar */}
         <Topbar onMenuClick={() => setSidebarOpen(true)} />
 
-        <div className="flex-1">
-  <PageContainer>
-    <Outlet />
-  </PageContainer>
-</div>
+        {/* Page Content */}
+        <div className="flex-1 overflow-auto">
+          <PageContainer>
+            <Outlet />
+          </PageContainer>
+        </div>
       </div>
     </div>
   );
